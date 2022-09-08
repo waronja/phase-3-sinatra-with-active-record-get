2e7cdd485975f728302029220c17b764e3fba9dd
simon waronja <simon.waronja@student.moringaschool.com>
2022-09-08 07:39:27 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,34 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  #Set content type header for all responses'
+  set :default_content_type, 'application/json'
+    
+
+  get '/games' do
+    #get all the gamws from the database
+    games = Game.all.order(:title).limit(10)
+
+    #return a JSON response with an array of all the game data
+    games.to_json
+
+  end
+
+  #Use :id to get a dynamic route
+
+  get '/games/:id' do
+    #Look up the game in the database using its ID
+    game = Game.find(params[:id])
+    #Send a JSON formatted response of the game data & include associated date in the response
+
+    #game.to_json(include: { reviews: { include: :user}})
+
+    #We can be more selective on the attributes returned from each model with only option
+
+    game.to_json(only: [:id, :title, :genre, :price], include: { 
+      reviews: { only: [:comment, :score], include: {
+         user: {only: [:name]}
+         }}
+        })
   end
 
 end
